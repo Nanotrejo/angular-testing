1ceed3e18263446f8a160105e6d4307ee8e13f73
Nanotrejo <nanotrejo@gmail.com>
2021-12-23 20:12:31 +0100

    App.Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+import { IncrementadorComponent } from './intermedio-2/incrementador/incrementador.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MedicosComponent } from './intermedio/espias/medicos.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MedicoComponent } from './intermedio-2/medico/medico.component';
+import { HospitalComponent } from './intermedio-2/hospital/hospital.component';
+import { RouterModule } from '@angular/router';
+import { RUTAS } from './avanzado/app.routes';
+import { NavbarComponent } from './avanzado/navbar/navbar.component';
+import { RouterMedicoComponent } from './avanzado/router-medico/router-medico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicosComponent,
+    MedicoComponent,
+    HospitalComponent,
+    IncrementadorComponent,
+    NavbarComponent,
+    RouterMedicoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(RUTAS)
   ],
   providers: [],
   bootstrap: [AppComponent]
